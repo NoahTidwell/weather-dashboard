ec6a17754bd794c7775ca89f053f67e6e77cd2d3
Noah Tidwell <ntidwell34@yahoo.com>
2021-10-03 21:47:08 -0500

    JavaScript for temp and city name display

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,23 @@ var displayCity = function () {
     createListEl.textContent = cityInputText;
     displaySearchedCity.appendChild(createListEl);
 
+    var displayCurrentTemp = document.getElementById("currentTemp");
+    var displayCityName = document.getElementById("citySearch");
+
+    
+
+
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityInputText + "&APPID=b30096dc99b5b0d5c4edfccb15cd2965")
+    .then(response => response.json())
+    .then(function(data) {
+        var cityName = data.name;
+        cityName.classList = "text-center"
+        displayCityName.textContent = cityName;
+        var temp = "Current Temp:" + " " + Math.floor((data.main.temp - 273.15) * 9/5 + 32) + "°F";
+        displayCurrentTemp.append(temp);
+    })
 }
+// Fetch API DATA
 
 searchBtn.addEventListener("click", displayCity);
